42f228a1eb8a9b45e19c751f3d12c11486621eff
angelalvcam <fluttershady@gmail.com>
2025-02-23 21:27:08 -0600

    added cascade structure to the matrix algorithm

--- view.ts
+++ view.ts
@@ -7,8 +7,8 @@ export class ExampleView extends ItemView {
   private intervalId: number | undefined;
   private ROWS: number;
   private COLS: number;
-  private lines: string[];
-  private binary: number[][];
+  private header: number[];
+  private terminal: number[][];
   private plugin: ExamplePlugin;
 
   constructor(leaf: WorkspaceLeaf, plugin: ExamplePlugin) {
@@ -26,70 +26,46 @@ export class ExampleView extends ItemView {
 
   addLine() {
     const container = this.containerEl.children[1];
-    let current_binary = this.generateRandomArray(this.COLS);
-    this.binary.push(current_binary);
-    this.lines.push(""); // Dummy placeholder
 
-    let local_count: number = 0;
-    
+    // Update header
+    for (let i = 0; i < this.COLS; i++) {
+      if (this.header[i] == 0 && Math.random() > 0.9) {
+        this.header[i] = Math.floor(Math.random() * (this.COLS - (this.COLS / 2) + 1)) + (this.COLS / 2);
+      }
+    }
+
+    let top_row: number[] = new Array(this.COLS).fill(0);
+    for (let j = 0; j < this.COLS; j++) {
+      if (this.header[j] > 0) {
+        top_row[j] = 1;
+        this.header[j]--;
+      }
+    }
+    this.terminal.unshift(top_row);
+    this.terminal.pop();
+
+
+    // Print terminal in the view
     container.empty();
     const matrix = container.createEl("div", { cls: "matrix" });
-    for (let i = this.binary.length - 1; i >= 0; i--) {
-      local_count++;
-      let current_binary = this.binary[i];
-    
-      // Ensure that i - 1 is >= 0 to prevent accessing undefined
-      let previous_binary = (i - 1 >= 0) ? this.binary[i - 1] : new Array(this.COLS).fill(0);
-      let previous_string = (i - 1 >= 0) ? this.lines[i - 1] : " ".repeat(this.COLS);
-      let current_string = this.merge(previous_binary, current_binary, previous_string);
-      this.lines[i] = current_string;
-
-      if (local_count > this.ROWS) {
-        break;
-      }
-
-      matrix.createEl("div", { text: this.lines[i], cls: "line" });
+    for (let i = 0; i < this.ROWS; i++) {
+      matrix.createEl("div", { text: this.toString(this.terminal[i]), cls: "line" });
     }
-
-    // Remove items that can't be printed
-    while (this.binary.length > this.ROWS) {
-      this.binary.shift();
-      this.lines.shift();
-    }
-
   }
 
-  andOperation(arr1: number[], arr2: number[]): number[] {
-    return arr1.map((val, i) => (val === 1 && arr2[i] === 1 ? 1 : 0));
-  }
-
-  generateRandomArray(length: number): number[] {
-    return Array.from({ length }, () => (Math.random() < 0.7 ? 0 : 1));
-  }
-
-  getRandomAlphanumeric(): string {
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const randomIndex = Math.floor(Math.random() * chars.length);
-    return chars[randomIndex];
-  }
-
-  merge(previous_binary: number[], new_binary: number[], old_string: string): string {
-    let output_string = "";
-    for (let i = 0; i < this.COLS; i++) {
-      if (previous_binary[i] == 1 && new_binary[i] == 1) {
-        output_string += old_string[i];
-      } else if (new_binary[i] == 1) {
-        output_string += this.getRandomAlphanumeric();
+  toString(binary: number[]) {
+    let out = "";
+    for (let i = 0; i < this.ROWS; i++) {
+      if (binary[i] == 1) {
+        out += "%";
       } else {
-        output_string += ' ';
+        out += " ";
       }
     }
-    return output_string;
+    return out;
   }
 
-  restart() {
-    this.binary = Array.from({ length: this.ROWS }, () => Array(this.COLS).fill(0)); 
-    this.lines = Array.from({ length: this.ROWS }, () => " ".repeat(this.COLS)); 
+  initialize() {
   }
 
   async onOpen() {
@@ -100,9 +76,11 @@ export class ExampleView extends ItemView {
     this.ROWS = this.plugin.settings.matrixRows;
     this.COLS = this.plugin.settings.matrixCols;
 
-    // Initialize the lists
-    this.binary = Array.from({ length: this.ROWS }, () => Array(this.COLS).fill(0)); 
-    this.lines = Array.from({ length: this.ROWS }, () => " ".repeat(this.COLS)); 
+    // Set the header
+    this.header = new Array(this.COLS).fill(0);
+
+    // Set the terminal
+    this.terminal = Array.from({ length: this.ROWS }, () => new Array(this.COLS).fill(0));
 
     // Store the interval ID
     this.intervalId = window.setInterval(() => {
